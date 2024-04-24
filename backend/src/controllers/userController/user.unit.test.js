f2c1f1c8f0b3373bbdb6b48c7df3ecc8f2effa95
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-04-24 11:50:08 +0200

    add test getAll in user controller

--- backend/src/controllers/userController/user.unit.test.js
+++ backend/src/controllers/userController/user.unit.test.js
@@ -1,18 +1,19 @@
-const { signup } = require('./user');
+const { signup, getAll } = require('./user');
 const {signupFactory} = require('./userFactory');
 const User = require('../../models/user');
+const { TECHNICIAN, PARTNER } = require('../../conf/role');
 
 // Mock di signupFactory e User
 jest.mock('./userFactory');
 jest.mock('../../models/user');
 
 // Test di signup
-describe('Signup', () => {
+describe('TEST signup', () => {
     let mockReq, mockRes, mockNext;
 
     beforeEach(() => {
     mockReq = {
-        params: { role: 'partner' },
+        params: { role: PARTNER },
         body: {
             email: 'example@example.com',
             password: 'longenoughpassword',
@@ -36,7 +37,7 @@ it('Creazione partner con successo', async () => {
     const newUser = {
         email: 'example@example.com',
         password: 'longenoughpassword',
-        role: 'partner'
+        role: PARTNER
     };
     User.signup.mockResolvedValue(newUser);
     // mock della funzione factory
@@ -53,7 +54,7 @@ it('Creazione tecnico con successo', async () => {
     const newUser = {
         email: 'example@example.com',
         password: 'longenoughpassword',
-        role: 'tecnico'
+        role: TECHNICIAN
     };
     // eliminazione del codice univoco per il tecnico
     delete newUser.codiceUnivoco;
@@ -92,3 +93,66 @@ it('ruolo non specificato', async () => {
     expect(mockRes.json).toHaveBeenCalledWith({ error: errorMessage });
   });
 });
+// end signup test
+
+// TEST getAll
+describe('TEST getAll', () => {
+    let mockReq, mockRes, mockNext;
+  
+    beforeEach(() => {
+      mockReq = {
+        params: {}
+      };
+      mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+      mockNext = jest.fn();
+      User.find = jest.fn();
+    });
+  
+    it('ritorna tutti i partenr con successo', async () => {
+      mockReq.params.role = PARTNER;
+      const mockUsers = [{ role: PARTNER }];
+      User.find.mockResolvedValue(mockUsers);
+      
+      await getAll(mockReq, mockRes);
+      
+      expect(User.find).toHaveBeenCalledWith({ role: PARTNER });
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith({ users: mockUsers });
+    });
+  
+    it('ritorna tutti i tecnici con successo', async () => {
+      mockReq.params.role = TECHNICIAN;
+      const mockUsers = [{ role: TECHNICIAN }];
+      User.find.mockResolvedValue(mockUsers);
+      
+      await getAll(mockReq, mockRes);
+      
+      expect(User.find).toHaveBeenCalledWith({ role: TECHNICIAN });
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith({ users: mockUsers });
+    });
+  
+    it('ruolo non supportato', async () => {
+      mockReq.params.role = 'admin';
+      
+      await getAll(mockReq, mockRes);
+      
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.json).toHaveBeenCalledWith({ error: "ruolo non supportato" });
+    });
+  
+    it('errore nel metodo User.find', async () => {
+      mockReq.params.role = PARTNER;
+      const errorMessage = 'Database error';
+      User.find.mockRejectedValue(new Error(errorMessage));
+      
+      await getAll(mockReq, mockRes);
+      
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.json).toHaveBeenCalledWith({ error: errorMessage });
+    });
+  });
+  

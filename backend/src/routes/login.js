df4c9b2834e80e65ee2b892c937fdd8df0fb5ff1
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-04-25 11:26:00 +0200

    update login routest import

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const { loginUser } = require('../controllers/authController/login');
+const { errorHandler } = require('../middleware/errorManager');
 
 const router = express.Router();
 
 // login route
 router.post('/', loginUser);
+
+router.use(errorHandler);
 
 module.exports = router;

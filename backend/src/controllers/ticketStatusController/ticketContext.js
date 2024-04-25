24738c4a758bb1b823cb37307f4aa4addbdf636f
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-04-25 21:51:52 +0200

    fix ticketContext

--- backend/src/controllers/ticketStatusController/ticketContext.js
+++ backend/src/controllers/ticketStatusController/ticketContext.js
@@ -1,26 +1,26 @@
 class TicketContext {
-    constructor(state) {
-        this.state = state;
-        this.transitionTo(state);
-    }
+  constructor(state) {
+    this.state = state;
+    this.transitionTo(state);
+  }
 
-    // permette di passare a una nuova istanza di TicketStatus
-    transitionTo(state) {
-        //console.log(state);
-        this.state = state;
-        this.state.setContext(this)
-    }
+  // permette di passare a una nuova istanza di TicketStatus
+  transitionTo(state) {
+    //console.log(state);
+    this.state = state;
+    this.state.setContext(this);
+  }
 
-    // permette di eseguire action in base all'istanza di TicketStatus
-    print(action) {
-        this.state.printNext(action);
-    }
+  // permette di eseguire action in base all'istanza di TicketStatus
+  print(action) {
+    this.state.printNext(action);
+  }
 
-    action(action) {
-        this.state.action(action);
-    }
+  action(action) {
+    this.state.handleAction(action);
+  }
 }
 
 module.exports = {
-    TicketContext,
-}
+  TicketContext,
+};

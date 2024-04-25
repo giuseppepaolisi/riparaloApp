ca8e34c55a58bb6e0d87c3ed66da0cf268b0c668
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-04-25 21:52:07 +0200

    run format

--- backend/src/controllers/ticketStatusController/ticketState.js
+++ backend/src/controllers/ticketStatusController/ticketState.js
@@ -1,291 +1,287 @@
 const {
-    APERTO,
-    ACCETTATO,
-    RITIRATO,
-    IN_LAVORAZIONE,
-    ATTESA_CONFERMA_PREVENTIVO,
-    PREVENTIVO_ACCETTATO,
-    PREVENTIVO_RIFIUTATO,
-    COMPLETATO,
-    ANNULLATO,
-    ATTESA_RICAMBIO,
-    IN_CONSEGNA_COMPLETATO,
-    IN_CONSEGNA_ANNULLATO,
-    IN_CONSEGNA_RIFIUTATO,
-  } = require('../../conf/state');
+  APERTO,
+  ACCETTATO,
+  RITIRATO,
+  IN_LAVORAZIONE,
+  ATTESA_CONFERMA_PREVENTIVO,
+  PREVENTIVO_ACCETTATO,
+  PREVENTIVO_RIFIUTATO,
+  COMPLETATO,
+  ANNULLATO,
+  ATTESA_RICAMBIO,
+  IN_CONSEGNA_COMPLETATO,
+  IN_CONSEGNA_ANNULLATO,
+  IN_CONSEGNA_RIFIUTATO,
+} = require('../../conf/state');
 
 // State interface
 class TicketState {
-    setContext(context) {
-        this.context = context;
-    }
+  setContext(context) {
+    this.context = context;
+  }
 
-    printNext(state) {
-    }
+  printNext(state) {}
 
-    handleAction(state) {
-
-    }
-
+  handleAction(state) {}
 }
 
 // Stato APERTO
-class OpenTicket extends TicketState { 
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === ACCETTATO) {
-            console.log(ACCETTATO);
-            this.context.transitionTo(new AcceptedTicket());
-        } else {
-            // transizione di stato invalida
-        }
+class OpenTicket extends TicketState {
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === ACCETTATO) {
+      console.log(ACCETTATO);
+      this.context.transitionTo(new AcceptedTicket());
+    } else {
+      // transizione di stato invalida
     }
+  }
 
-    handleAction(state) {
-        if(state === ACCETTATO) {
-            // transizione valisa
-            return true;
-        }
-        // transizione di stato invalida
-        return false;
+  handleAction(state) {
+    if (state === ACCETTATO) {
+      // transizione valisa
+      return true;
     }
+    // transizione di stato invalida
+    return false;
+  }
 }
 
 // Stato ACCETTATO
 class AcceptedTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === RITIRATO) {
-            console.log(RITIRATO);
-            this.context.transitionTo(new RetrivedTicket());
-        } else if(state === IN_LAVORAZIONE) {
-            console.log(IN_LAVORAZIONE);
-            this.context.transitionTo(new InProgressTicket()); 
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === RITIRATO) {
+      console.log(RITIRATO);
+      this.context.transitionTo(new RetrivedTicket());
+    } else if (state === IN_LAVORAZIONE) {
+      console.log(IN_LAVORAZIONE);
+      this.context.transitionTo(new InProgressTicket());
     }
-    
-    handleAction(state) {
-        if(state === RITIRATO) {
-            // transizione valisa
-            return true;
-        } else if(state === IN_LAVORAZIONE){
-            // transizione di stato invalida
-            return true;
-        }
-        return false;
+  }
+
+  handleAction(state) {
+    if (state === RITIRATO) {
+      // transizione valisa
+      return true;
+    } else if (state === IN_LAVORAZIONE) {
+      // transizione di stato invalida
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato RITIRATO
 class RetrivedTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === IN_LAVORAZIONE) {
-            console.log(IN_LAVORAZIONE);
-            this.context.transitionTo(new InProgressTicket());
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === IN_LAVORAZIONE) {
+      console.log(IN_LAVORAZIONE);
+      this.context.transitionTo(new InProgressTicket());
     }
+  }
 
-    handleAction(state) {
-        if(state === IN_LAVORAZIONE) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === IN_LAVORAZIONE) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato IN LAVORAZIONE
 class InProgressTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === ATTESA_CONFERMA_PREVENTIVO) {
-            console.log(ATTESA_CONFERMA_PREVENTIVO);
-            this.context.transitionTo(new AwatingQuoteConfirmTicket());
-        } else if(state === ANNULLATO) {
-            console.log(ANNULLATO);
-            this.context.transitionTo(new CancelledTicket());
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === ATTESA_CONFERMA_PREVENTIVO) {
+      console.log(ATTESA_CONFERMA_PREVENTIVO);
+      this.context.transitionTo(new AwatingQuoteConfirmTicket());
+    } else if (state === ANNULLATO) {
+      console.log(ANNULLATO);
+      this.context.transitionTo(new CancelledTicket());
     }
+  }
 
-    handleAction(state) {
-        if(state === ATTESA_CONFERMA_PREVENTIVO) {
-            return true;
-        } else if(state === ANNULLATO) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === ATTESA_CONFERMA_PREVENTIVO) {
+      return true;
+    } else if (state === ANNULLATO) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato ATTESA CONFERMA PREVENTIVO
 class AwatingQuoteConfirmTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === PREVENTIVO_ACCETTATO) {
-            this.context.transitionTo(new QuoteAcceptedTicket());
-            console.log(PREVENTIVO_ACCETTATO);
-        } else if(state === PREVENTIVO_RIFIUTATO) {
-            this.context.transitionTo(new QuoteRejectedTicket());
-            console.log(PREVENTIVO_RIFIUTATO);
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === PREVENTIVO_ACCETTATO) {
+      this.context.transitionTo(new QuoteAcceptedTicket());
+      console.log(PREVENTIVO_ACCETTATO);
+    } else if (state === PREVENTIVO_RIFIUTATO) {
+      this.context.transitionTo(new QuoteRejectedTicket());
+      console.log(PREVENTIVO_RIFIUTATO);
     }
+  }
 
-    handleAction(state) {
-        if(state === PREVENTIVO_ACCETTATO) {
-            return true;
-        } else if(state === PREVENTIVO_RIFIUTATO) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === PREVENTIVO_ACCETTATO) {
+      return true;
+    } else if (state === PREVENTIVO_RIFIUTATO) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato PREVENTIVO ACCETTATO
 class QuoteAcceptedTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === COMPLETATO) {
-            this.context.transitionTo(new CompletedTicket());
-            console.log(COMPLETATO);
-        } else if(state === ATTESA_RICAMBIO) {
-            this.context.transitionTo(new AwationgPartsTicket());
-            console.log(ATTESA_RICAMBIO);
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === COMPLETATO) {
+      this.context.transitionTo(new CompletedTicket());
+      console.log(COMPLETATO);
+    } else if (state === ATTESA_RICAMBIO) {
+      this.context.transitionTo(new AwationgPartsTicket());
+      console.log(ATTESA_RICAMBIO);
     }
+  }
 
-    handleAction(state) {
-        if(state === COMPLETATO) {
-            return true;
-        } else if(state === ATTESA_RICAMBIO) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === COMPLETATO) {
+      return true;
+    } else if (state === ATTESA_RICAMBIO) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato ATTESA RICAMBIO
 class AwationgPartsTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === COMPLETATO) {
-            this.context.transitionTo(new CompletedTicket());
-            console.log(COMPLETATO);
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === COMPLETATO) {
+      this.context.transitionTo(new CompletedTicket());
+      console.log(COMPLETATO);
     }
+  }
 
-    handleAction(state) {
-        if(state === COMPLETATO) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === COMPLETATO) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato COMPLETATO
 class CompletedTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === IN_CONSEGNA_COMPLETATO) {
-            this.context.transitionTo(new InDdeliveryCompletedTicket());
-            console.log(IN_CONSEGNA_COMPLETATO);
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === IN_CONSEGNA_COMPLETATO) {
+      this.context.transitionTo(new InDdeliveryCompletedTicket());
+      console.log(IN_CONSEGNA_COMPLETATO);
     }
+  }
 
-    handleAction(state) {
-        if(state === IN_CONSEGNA_COMPLETATO) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === IN_CONSEGNA_COMPLETATO) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato IN CONSEGNA COMPLETATO
 class InDdeliveryCompletedTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        // stato finale pos
-        console.log(IN_CONSEGNA_COMPLETATO);
-    }
+  printNext(state) {
+    console.log('state: ' + state);
+    // stato finale pos
+    console.log(IN_CONSEGNA_COMPLETATO);
+  }
 
-    handleAction(state) {
-        return true;
-    }
+  handleAction(state) {
+    return true;
+  }
 }
 
 // Stato ANNULLATO
 class CancelledTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === IN_CONSEGNA_ANNULLATO) {
-            this.context.transitionTo(new InDdeliveryCancelledTicket());
-            console.log(IN_CONSEGNA_ANNULLATO);
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === IN_CONSEGNA_ANNULLATO) {
+      this.context.transitionTo(new InDdeliveryCancelledTicket());
+      console.log(IN_CONSEGNA_ANNULLATO);
     }
+  }
 
-    handleAction(state) {
-        if(state === IN_CONSEGNA_ANNULLATO) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === IN_CONSEGNA_ANNULLATO) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato IN CONSEGNA ANNULLATO
 class InDdeliveryCancelledTicket extends TicketState {
-    printNext(state) {
-        // stato finale
-        console.log("state: " + state);
-        console.log(IN_CONSEGNA_ANNULLATO);
-    }
+  printNext(state) {
+    // stato finale
+    console.log('state: ' + state);
+    console.log(IN_CONSEGNA_ANNULLATO);
+  }
 
-    handleAction(state) {
-        return true;
-    }
+  handleAction(state) {
+    return true;
+  }
 }
 
 // Stato PREVENTIVO RIFIUTATO
 class QuoteRejectedTicket extends TicketState {
-    printNext(state) {
-        console.log("state: " + state);
-        if(state === IN_CONSEGNA_RIFIUTATO) {
-            this.context.transitionTo(new InDdeliveryRejectedTicket());
-            console.log(IN_CONSEGNA_RIFIUTATO);
-        }
+  printNext(state) {
+    console.log('state: ' + state);
+    if (state === IN_CONSEGNA_RIFIUTATO) {
+      this.context.transitionTo(new InDdeliveryRejectedTicket());
+      console.log(IN_CONSEGNA_RIFIUTATO);
     }
+  }
 
-    handleAction(state) {
-        if(state === IN_CONSEGNA_RIFIUTATO) {
-            return true;
-        }
-        return false;
+  handleAction(state) {
+    if (state === IN_CONSEGNA_RIFIUTATO) {
+      return true;
     }
+    return false;
+  }
 }
 
 // Stato IN CONSEGNA RIFIUTATO
 class InDdeliveryRejectedTicket extends TicketState {
-    printNext(state) {
-        // stato finale
-        console.log("state: " + state);
-        console.log(IN_CONSEGNA_RIFIUTATO);
-    }
+  printNext(state) {
+    // stato finale
+    console.log('state: ' + state);
+    console.log(IN_CONSEGNA_RIFIUTATO);
+  }
 
-    handleAction(state) {
-        return true;
-    }
+  handleAction(state) {
+    return true;
+  }
 }
 
 module.exports = {
-    OpenTicket,
-    AcceptedTicket,
-    RetrivedTicket,
-    InProgressTicket,
-    AwatingQuoteConfirmTicket,
-    QuoteAcceptedTicket,
-    AwationgPartsTicket,
-    CompletedTicket,
-    InDdeliveryCompletedTicket,
-    CancelledTicket,
-    InDdeliveryCancelledTicket,
-    QuoteRejectedTicket,
-    InDdeliveryRejectedTicket,
-}
+  OpenTicket,
+  AcceptedTicket,
+  RetrivedTicket,
+  InProgressTicket,
+  AwatingQuoteConfirmTicket,
+  QuoteAcceptedTicket,
+  AwationgPartsTicket,
+  CompletedTicket,
+  InDdeliveryCompletedTicket,
+  CancelledTicket,
+  InDdeliveryCancelledTicket,
+  QuoteRejectedTicket,
+  InDdeliveryRejectedTicket,
+};

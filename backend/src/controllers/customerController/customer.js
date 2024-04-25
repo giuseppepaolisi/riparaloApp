aaee18b280edd5749154630a415d04b8effcc0d1
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-04-25 12:55:32 +0200

    add customer controller

---
+++ backend/src/controllers/customerController/customer.js
@@ -0,0 +1,44 @@
+const Customer = require('../../models/customer');
+const { ErrorResponse } = require('../../middleware/errorManager');
+
+const createCustomer = async (req, res, next) => {
+  const { email, nome, cognome, telefono } = req.body;
+
+  try {
+    // validazione input
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      return next(new ErrorResponse("Inserire un'email valida", 400));
+    }
+    if (!nome) {
+      return next(new ErrorResponse('Inserire un nome', 400));
+    }
+    if (!cognome) {
+      return next(new ErrorResponse('Inserire un cognome', 400));
+    }
+    if (!telefono && telefono.lenght < 10) {
+      return next(
+        new ErrorResponse('Inserire un numero di telefono valido', 400)
+      );
+    }
+
+    const customer = await Customer.create({ email, nome, cognome, telefono });
+    res.status(201).json({ customer });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getCustomers = async (req, res, next) => {
+  try {
+    const customers = await Customer.find({});
+
+    res.status(200).json({ customers });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createCustomer,
+  getCustomers,
+};

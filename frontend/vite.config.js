67917c5cc43244d88c79ad9823c2fc4bf9ab40f9
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-05-04 18:30:53 +0200

    Add proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {  // Tutte le richieste che iniziano con '/api' verranno proxyate
+        target: 'http://localhost:4000',  // URL del server
+        changeOrigin: true,  // Necessario per i virtual host
+        secure: false,  // Se il backend è in https, impostare su true
+      }
+    }
+  }
+});

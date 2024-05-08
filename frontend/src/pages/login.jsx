bb20bd3385f0266a6ac134c5aa9788a91305fdde
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-05-08 12:01:43 +0200

    fix login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -48,73 +48,75 @@ const LoginPage = () => {
   }, []);
 
   return (
-    <div className="row justify-content-center">
-      <div className="col-xl-6 col-lg-7 col-md-5">
-        <div className="card o-hidden border-0 shadow-lg my-5">
-          <div className="card-body p-0">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="p-1">
-                  <div className="text-center">
-                    <h1 className="h4 text-gray-900 mb-4">Benvenuto!</h1>
-                  </div>
-                  <form className="user" onSubmit={handleLogin}>
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        className="form-control form-control-user"
-                        id="exampleInputEmail"
-                        aria-describedby="emailHelp"
-                        placeholder="Indirizzo email..."
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                      />
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-xl-6 col-lg-7 col-md-5">
+          <div className="card o-hidden border-0 shadow-lg my-5">
+            <div className="card-body p-0">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="p-1">
+                    <div className="text-center">
+                      <h1 className="h4 text-gray-900 mb-4">Benvenuto!</h1>
                     </div>
-                    <div className="form-group">
-                      <input
-                        type="password"
-                        className="form-control form-control-user"
-                        id="exampleInputPassword"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                      />
-                    </div>
-                    <div className="form-group">
-                      <div className="custom-control custom-checkbox small">
+                    <form className="user" onSubmit={handleLogin}>
+                      <div className="form-group">
                         <input
-                          type="checkbox"
-                          className="custom-control-input"
-                          id="customCheck"
+                          type="email"
+                          className="form-control form-control-user"
+                          id="exampleInputEmail"
+                          aria-describedby="emailHelp"
+                          placeholder="Indirizzo email..."
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          required
                         />
-                        <label
-                          className="custom-control-label"
-                          htmlFor="customCheck"
-                        >
-                          Ricordami
-                        </label>
                       </div>
-                    </div>
-                    <button
-                      type="submit"
-                      className="btn btn-primary btn-user btn-block"
-                    >
-                      Login
-                    </button>
-                  </form>
-                  {error ? (
-                    <div
-                      className="alert alert-danger"
-                      style={{ marginTop: "20px" }}
-                      role="alert"
-                    >
-                      {error}
-                    </div>
-                  ) : (
-                    <div></div>
-                  )}
+                      <div className="form-group">
+                        <input
+                          type="password"
+                          className="form-control form-control-user"
+                          id="exampleInputPassword"
+                          placeholder="Password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <div className="custom-control custom-checkbox small">
+                          <input
+                            type="checkbox"
+                            className="custom-control-input"
+                            id="customCheck"
+                          />
+                          <label
+                            className="custom-control-label"
+                            htmlFor="customCheck"
+                          >
+                            Ricordami
+                          </label>
+                        </div>
+                      </div>
+                      <button
+                        type="submit"
+                        className="btn btn-primary btn-user btn-block"
+                      >
+                        Login
+                      </button>
+                    </form>
+                    {error ? (
+                      <div
+                        className="alert alert-danger"
+                        style={{ marginTop: "20px" }}
+                        role="alert"
+                      >
+                        {error}
+                      </div>
+                    ) : (
+                      <div></div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>

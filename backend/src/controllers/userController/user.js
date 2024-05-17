8a7fbd922600f62f24ca1d423f4b18dd49dd6db3
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-05-17 18:21:34 +0200

    Fix user

--- backend/src/controllers/userController/user.js
+++ backend/src/controllers/userController/user.js
@@ -8,14 +8,14 @@ const { PARTNER, TECHNICIAN } = require('../../conf/role');
 const mongoose = require('mongoose');
 const { ErrorResponse } = require('../../middleware/errorManager');
 
-// Si occupa della creazione di untente
+// Si occupa della creazione di utente
 const signup = async (req, res, next) => {
   const { role } = req.params;
 
   try {
     let newuser = req.body;
 
-    // Controlli comuni (es. validazione email e password)
+    // validazione email e password
     if (!/\S+@\S+\.\S+/.test(newuser.email)) {
       return next(new ErrorResponse("Inserire un'email valida", 400));
     }

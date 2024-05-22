5eee6a418a35095334d20e1179243a5abe86c5df
KrisTwinn <djchris.cg@gmail.com>
2024-05-22 13:31:15 +0200

    Add AboutUs

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap";
+
+function AboutUs() {
+  return (
+    <div className="sidebar-page">
+      <Container>
+        <h1>About Us</h1>
+        <p>
+          Welcome to our application. We are dedicated to providing the best
+          service possible.
+        </p>
+        <p>
+          Our team consists of experienced professionals who are passionate
+          about what they do.
+        </p>
+        <p>Thank you for choosing us. We hope you have a great experience!</p>
+      </Container>
+    </div>
+  );
+}
+
+export default AboutUs;

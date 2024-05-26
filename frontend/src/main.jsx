d550469fd3bd6bd4ab16a96d116b159daabd9a0e
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-05-26 20:38:47 +0200

    Add Redux auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import App from "./App.jsx";
+import store from "./redux/auth/store.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
---
+++ frontend/src/redux/auth/slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isAuthenticated = true;
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.token = null;
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export const selectAuth = (state) => state.auth;
+export default authSlice.reducer;

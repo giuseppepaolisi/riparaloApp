eeeeea66c78b1c745873531c81e9691040e86c52
KrisTwinn <djchris.cg@gmail.com>
2024-06-02 21:40:30 +0200

    Fix AggiungiModello

--- frontend/src/pages/Admin/AggiungiModello.jsx
+++ frontend/src/pages/Admin/AggiungiModello.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState, useCallback } from "react";
+import FormInput from "../../components/FormInput";
 
 const AggiungiModello = () => {
-  const [brand, setBrand] = useState("");
-  const [model, setModel] = useState("");
+  const [formData, setFormData] = useState({
+    brand: "",
+    model: "",
+    newServiceName: "",
+    newServicePrice: "",
+  });
   const [services, setServices] = useState([]);
-  const [newServiceName, setNewServiceName] = useState("");
-  const [newServicePrice, setNewServicePrice] = useState("");
 
   useEffect(() => {
     document.body.style.backgroundColor = "#007bff";
@@ -15,31 +18,47 @@ const AggiungiModello = () => {
     };
   }, []);
 
+  const handleChange = useCallback((e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  }, []);
+
   const addService = () => {
+    const { newServiceName, newServicePrice } = formData;
     if (newServiceName && newServicePrice) {
       setServices([
         ...services,
         { name: newServiceName, price: newServicePrice },
       ]);
-      setNewServiceName("");
-      setNewServicePrice("");
+      setFormData((prevData) => ({
+        ...prevData,
+        newServiceName: "",
+        newServicePrice: "",
+      }));
     }
   };
 
   const saveModel = () => {
     const modelData = {
-      brand,
-      model,
+      brand: formData.brand,
+      model: formData.model,
       services,
     };
     console.log("Saved Model Data:", modelData);
-    setBrand("");
-    setModel("");
+    setFormData({
+      brand: "",
+      model: "",
+      newServiceName: "",
+      newServicePrice: "",
+    });
     setServices([]);
   };
 
   const isSaveButtonDisabled = () => {
-    return !(brand && model && services.length > 0);
+    return !(formData.brand && formData.model && services.length > 0);
   };
 
   return (
@@ -52,58 +71,46 @@ const AggiungiModello = () => {
               <form>
                 <div className="row mb-3">
                   <div className="col-md-6">
-                    <label htmlFor="brand" className="form-label">
-                      Brand
-                    </label>
-                    <input
+                    <FormInput
+                      label="Brand"
                       type="text"
-                      className="form-control"
+                      value={formData.brand}
+                      onChange={handleChange}
                       id="brand"
-                      value={brand}
-                      onChange={(e) => setBrand(e.target.value)}
-                      style={{ maxWidth: "300px" }}
                       required
+                      style={{ maxWidth: "300px" }}
                     />
                   </div>
                   <div className="col-md-6">
-                    <label htmlFor="model" className="form-label">
-                      Modello
-                    </label>
-                    <input
+                    <FormInput
+                      label="Modello"
                       type="text"
-                      className="form-control"
+                      value={formData.model}
+                      onChange={handleChange}
                       id="model"
-                      value={model}
-                      onChange={(e) => setModel(e.target.value)}
-                      style={{ maxWidth: "300px" }}
                       required
+                      style={{ maxWidth: "300px" }}
                     />
                   </div>
                 </div>
                 <div className="row mb-3">
                   <div className="col-md-6">
-                    <label htmlFor="serviceName" className="form-label">
-                      Nome Servizio
-                    </label>
-                    <input
+                    <FormInput
+                      label="Nome Servizio"
                       type="text"
-                      className="form-control"
-                      id="serviceName"
-                      value={newServiceName}
-                      onChange={(e) => setNewServiceName(e.target.value)}
+                      value={formData.newServiceName}
+                      onChange={handleChange}
+                      id="newServiceName"
                       style={{ maxWidth: "300px" }}
                     />
                   </div>
                   <div className="col-md-6">
-                    <label htmlFor="servicePrice" className="form-label">
-                      Prezzo Servizio
-                    </label>
-                    <input
+                    <FormInput
+                      label="Prezzo Servizio"
                       type="text"
-                      className="form-control"
-                      id="servicePrice"
-                      value={newServicePrice}
-                      onChange={(e) => setNewServicePrice(e.target.value)}
+                      value={formData.newServicePrice}
+                      onChange={handleChange}
+                      id="newServicePrice"
                       style={{ maxWidth: "300px" }}
                     />
                   </div>

4f96b62173ebb592178386086e17f584c5de58d6
KrisTwinn <djchris.cg@gmail.com>
2024-06-02 21:37:46 +0200

    Edit Homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,35 +1,48 @@
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import logo from "../assets/img/logo-riparalo.png"; // Assicurati che il percorso sia corretto
+import logo from "../assets/img/logo-riparalo.png";
 
 const HomePage = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   return (
     <div style={{ fontFamily: "Open Sans", color: "#009be3" }}>
-      <div className="d-flex justify-content-end p-3">
-        {!isAuthenticated && (
-          <Link to="/login">
-            <Button variant="outline-primary" size="lg">
-              Login
-            </Button>
-          </Link>
-        )}
-      </div>
-      <div className="text-center">
-        <h1>Benvenuto!</h1>
-        <div className="d-flex justify-content-center">
-          <img
-            src={logo}
-            className="img-fluid"
-            style={{ width: "40%" }}
-            alt="Logo"
-          />
-        </div>
-      </div>
+      <Header isAuthenticated={isAuthenticated} />
+      <Benvenuto />
     </div>
   );
 };
+
+const Header = ({ isAuthenticated }) => (
+  <div className="d-flex justify-content-end p-3">
+    {!isAuthenticated && (
+      <Link to="/login">
+        <Button variant="outline-primary" size="lg">
+          Login
+        </Button>
+      </Link>
+    )}
+  </div>
+);
+
+Header.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+};
+
+const Benvenuto = () => (
+  <div className="text-center">
+    <h1>Benvenuto!</h1>
+    <div className="d-flex justify-content-center">
+      <img
+        src={logo}
+        className="img-fluid"
+        style={{ width: "40%" }}
+        alt="Logo"
+      />
+    </div>
+  </div>
+);
 
 export default HomePage;

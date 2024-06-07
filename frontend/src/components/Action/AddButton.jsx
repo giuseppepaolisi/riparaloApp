e515ffe665a03b3ffe8d902d2d9e794b9a9c5bbf
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-06-07 17:45:15 +0200

    Fix AddButton style

--- frontend/src/components/Action/AddButton.jsx
+++ frontend/src/components/Action/AddButton.jsx
@@ -2,7 +2,10 @@ import PropTypes from "prop-types";
 
 export default function AddButton({ label, link }) {
   return (
-    <a href={link} className="btn btn-primary">
+    <a
+      href={link}
+      className="btn btn-primary d-flex align-items-center justify-content-center"
+    >
       + {label}
     </a>
   );

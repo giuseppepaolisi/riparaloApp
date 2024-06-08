54db89d655574cfa1def062cebe6c0d8e4191e40
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-06-08 11:34:04 +0200

    Add CustomAlert

---
+++ frontend/src/components/Alert/CustomAlert.jsx
@@ -0,0 +1,36 @@
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+export default function CustomAlert({ msg, severity }) {
+  const duration = 6000;
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOpen(false);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration]);
+
+  if (!open) return null;
+
+  return (
+    <div style={{ position: "fixed", top: 16, right: 16, zIndex: 1000 }}>
+      <Stack sx={{ width: "100%" }} spacing={2}>
+        <Alert severity={severity}>
+          <AlertTitle>{severity === "error" ? "Error" : "Success"}</AlertTitle>
+          {msg}
+        </Alert>
+      </Stack>
+    </div>
+  );
+}
+
+CustomAlert.propTypes = {
+  msg: PropTypes.string.isRequired,
+  severity: PropTypes.string.isRequired,
+};

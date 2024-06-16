534e1049047e511c57a695a853e72b318bf8c31b
KrisTwinn <djchris.cg@gmail.com>
2024-06-16 18:46:15 +0200

    Add theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import theme from "./theme.js";
 
 import App from "./App.jsx";
 import store from "./redux/auth/store.js";
+import { ThemeProvider } from "react-bootstrap";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </ThemeProvider>
 );

78785fdb4fce03e68719624180de41e117af4e56
KrisTwinn <djchris.cg@gmail.com>
2024-06-27 22:27:08 +0200

    Move theme.js in folder js and fix main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import theme from "./theme.js";
+import theme from "./assets/js/theme.js";
 
 import App from "./App.jsx";
 import store from "./redux/auth/store.js";

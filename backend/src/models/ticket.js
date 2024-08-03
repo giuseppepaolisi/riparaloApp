989c8bb335952796004d320f1b6219c51c23b4c4
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-08-03 10:56:47 +0200

    Add timestamp

--- backend/src/models/ticket.js
+++ backend/src/models/ticket.js
@@ -49,109 +49,112 @@ const servizioSchema = new mongoose.Schema(
 );
 
 // Schema principale per Ticket
-const ticketSchema = new mongoose.Schema({
-  id_partner: {
-    type: String,
-    required: true,
+const ticketSchema = new mongoose.Schema(
+  {
+    id_partner: {
+      type: String,
+      required: true,
+    },
+    telefono_partner: {
+      type: String,
+      required: true,
+    },
+    ragione_sociale: {
+      type: String,
+      required: true,
+    },
+    partita_iva: {
+      type: String,
+      required: true,
+    },
+    codiceUnivoco: {
+      type: String,
+      required: true,
+    },
+    pec: {
+      type: String,
+    },
+    cap: {
+      type: String,
+      required: true,
+    },
+    via: {
+      type: String,
+      required: true,
+    },
+    provincia: {
+      type: String,
+      required: true,
+    },
+    nome_cliente: {
+      type: String,
+      required: true,
+    },
+    cognome_cliente: {
+      type: String,
+      required: true,
+    },
+    telefono_cliente: {
+      type: String,
+      required: true,
+    },
+    marca: {
+      type: String,
+      required: true,
+    },
+    modello: {
+      type: String,
+      required: true,
+    },
+    descrizione_problema: {
+      type: String,
+      required: true,
+    },
+    descrizione_tecnica: {
+      type: String,
+    },
+    stato: {
+      type: String,
+      required: true,
+      enum: [
+        APERTO,
+        ACCETTATO,
+        RITIRATO,
+        IN_LAVORAZIONE,
+        ATTESA_CONFERMA_PREVENTIVO,
+        PREVENTIVO_ACCETTATO,
+        PREVENTIVO_RIFIUTATO,
+        COMPLETATO,
+        ANNULLATO,
+        ATTESA_RICAMBIO,
+        IN_CONSEGNA_COMPLETATO,
+        IN_CONSEGNA_ANNULLATO,
+        IN_CONSEGNA_RIFIUTATO,
+      ],
+    },
+    storico_stato: [storicoStatoSchema],
+    servizi: [servizioSchema],
+    prezzo_stimato: {
+      type: Number,
+    },
+    prezzo: {
+      type: Number,
+    },
+    acconto: {
+      type: Number,
+    },
+    imei: {
+      type: String,
+    },
+    pin: {
+      type: String,
+    },
+    seriale: {
+      type: String,
+    },
   },
-  telefono_partner: {
-    type: String,
-    required: true,
-  },
-  ragione_sociale: {
-    type: String,
-    required: true,
-  },
-  partita_iva: {
-    type: String,
-    required: true,
-  },
-  codiceUnivoco: {
-    type: String,
-    required: true,
-  },
-  pec: {
-    type: String,
-  },
-  cap: {
-    type: String,
-    required: true,
-  },
-  via: {
-    type: String,
-    required: true,
-  },
-  provincia: {
-    type: String,
-    required: true,
-  },
-  nome_cliente: {
-    type: String,
-    required: true,
-  },
-  cognome_cliente: {
-    type: String,
-    required: true,
-  },
-  telefono_cliente: {
-    type: String,
-    required: true,
-  },
-  marca: {
-    type: String,
-    required: true,
-  },
-  modello: {
-    type: String,
-    required: true,
-  },
-  descrizione_problema: {
-    type: String,
-    required: true,
-  },
-  descrizione_tecnica: {
-    type: String,
-  },
-  stato: {
-    type: String,
-    required: true,
-    enum: [
-      APERTO,
-      ACCETTATO,
-      RITIRATO,
-      IN_LAVORAZIONE,
-      ATTESA_CONFERMA_PREVENTIVO,
-      PREVENTIVO_ACCETTATO,
-      PREVENTIVO_RIFIUTATO,
-      COMPLETATO,
-      ANNULLATO,
-      ATTESA_RICAMBIO,
-      IN_CONSEGNA_COMPLETATO,
-      IN_CONSEGNA_ANNULLATO,
-      IN_CONSEGNA_RIFIUTATO,
-    ],
-  },
-  storico_stato: [storicoStatoSchema],
-  servizi: [servizioSchema],
-  prezzo_stimato: {
-    type: Number,
-  },
-  prezzo: {
-    type: Number,
-  },
-  acconto: {
-    type: Number,
-  },
-  imei: {
-    type: String,
-  },
-  pin: {
-    type: String,
-  },
-  seriale: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const Ticket = mongoose.model('Ticket', ticketSchema);
 
